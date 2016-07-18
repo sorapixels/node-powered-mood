b8db63073905294431c7a95f9249a534503c1205
Takao NAITO <xarsh@outlook.com>
2016-07-18 23:47:20 +0900

    Awesome.

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,9 @@
 <template>
   <h1>npm powered mood</h1>
   <p class="control has-addons">
-    <input class="input" type="text" placeholder="Noun">
-    <a class="button is-info">CHECK</a>
-    <div v-if="youDrink">
+    <input class="input" type="text" placeholder="Noun" v-model="packageName">
+    <a class="button is-info" v-on:click="findPackage">CHECK</a>
+    <div v-if="youDrink === true">
       <p>
         <img v-bind:src="drink">
         <h2>YOU DRINK!!</h2>
@@ -14,7 +14,7 @@
         <h4>{{ npm.stat }}</h4>
       </p>
     </div>
-    <div v-if="!youDrink">
+    <div v-if="youDrink === false">
       <p>
         <img v-bind:src="noDrink">
         <h2>NO DRINK</h2>
@@ -22,18 +22,25 @@
       <p>
         <h3>SHAME ON YOU</h3>
       </p>
+    </div>
+    <div v-if="youDrink === null">
+      <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Drinking game for npm users:<br>➀ Think of a noun<br>➁ npm install &lt;noun&gt;<br>➂ If it installs - drink!</p>&mdash; Sindre Sorhus (@sindresorhus) <a href="https://twitter.com/sindresorhus/status/515511151669805056">September 26, 2014</a></blockquote>
+    </div>
   </p>
+  <small><a href="https://sorapixels.com">Sorapixels</a></small>
 </template>
 
 <script>
+import finder from './find-package';
+
 export default {
   data () {
     return {
-      youDrink: false,
+      youDrink: null,
+      packageName: 'hello',
       npm: {
-        title: 'browserify',
-        summary: 'browser-side require() the node way',
-        stat: '2,182,692 downloads in the last month'
+        title: null,
+        summary: null
       }
     }
   },
@@ -43,6 +50,19 @@ export default {
     },
     noDrink () {
       return `/static/images/no-drink-${Math.floor(Math.random() * 5 + 1)}.gif`;
+    }
+  },
+  methods: {
+    findPackage () {
+      finder(this.packageName)
+      .then(res => {
+        this.youDrink = true
+        this.npm.title = res.name;
+        this.npm.summary = res.description;
+      })
+      .catch(err => {
+        this.youDrink = false;
+      });
     }
   }
 }
@@ -69,5 +89,14 @@ h3 {
 }
 h4 {
   font-size: 3vw;
+}
+small, small a {
+  color: #ccc;
+  font-size: 3vw;
+  margin-top: 12px;
+  display: inline-block;
+}
+.twitter-tweet.twitter-tweet-rendered {
+  margin: 0 auto;
 }
 </style>
